e94f2f44eed5b95ce3e2bdb37d65f8c89637e228
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2023-04-25 22:51:01 +0530

    Create index.js

---
+++ Tribute Page/index.js
@@ -0,0 +1,16 @@
+
+$(".one").hover(function(){
+    $(".one").addClass("hovered");
+},function(){
+    $(".one").removeClass("hovered");
+});
+$(".two").hover(function(){
+    $(".two").addClass("hovered");
+},function(){
+    $(".two").removeClass("hovered");
+});
+$(".three").hover(function(){
+    $(".three").addClass("hovered");
+},function(){
+    $(".three").removeClass("hovered");
+});
